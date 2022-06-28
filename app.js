b80d1131ebc7295b65d5512da9a020ebb10b6ee6
Tushar Anekar <92424004+TusharAnekar@users.noreply.github.com>
2022-06-28 13:31:36 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", clickHandler())
+btnTranslate.addEventListener("click", clickHandler)
 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" +text;
+}
+
+function errorHandler(error){
+    console.log("Error occured ", error)
+}
 function clickHandler() {
-        console.log("clicked");
-        console.log("input is", txtInput.value)
-    }
+        var inputText = txtInput.value;
+        fetch(getTranslationURL(inputText))
+        .then( Response => Response.json())
+        .then( json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
+        .catch(errorHandler)
+    
+        //outputDiv.innerText = "Translated " + txtInput.value;
+    } 
+
